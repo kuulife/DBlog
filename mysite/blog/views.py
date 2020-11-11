14a0adee528061ff164bbfa043e9a8a4230799a1
kutman <kutman@MacBook-Pro-kutman.local>
2020-11-12 00:03:46 +0600

    database added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def home(request):
-	return render(request, 'blog/home.html')
+	posts = Post.objects.all()
+	context = {
+	'posts':posts,
+	}
+	return render(request, 'blog/home.html',context)
 
 def about(request):
 	return render(request, 'blog/about.html')
