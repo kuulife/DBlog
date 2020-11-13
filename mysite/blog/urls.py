b9981c55dc3a73cc192c44b6c8d66ee13a47615f
kutman <kutman@MacBook-Pro-kutman.local>
2020-11-13 19:21:57 +0600

    commets

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,14 +6,17 @@ from .views import (
 	PostCreatView,
 	PostUpdateView,
 	PostDeletelView,
+	UserPostListView,
 )
 
 urlpatterns = [
 
 	path('',PostListView.as_view(), name='blog-home'),
+	path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
 	path('about/', views.about, name ='blog-about'),
 	path('post/<int:pk>',PostDetailView.as_view(), name = 'post-detail'),
 	path('post/new',PostCreatView.as_view(), name = 'post-create'),
 	path('post/<int:pk>/update',PostUpdateView.as_view(), name = 'post-update'),
 	path('post/<int:pk>/delete',PostDeletelView.as_view(), name = 'post-delete'),
+
 	]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,4 +133,15 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
+
 
